8fffe67b1c2c5efe95f3237cdf54eb1623ee7877
IlenaCaruk <locus93@bk.ru>
2019-04-17 15:58:51 +0300

    Class horizont

---
+++ HorizontallLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new2
+{
+    class HorizontallLine
+    {
+        List<Point> pList;
+
+        public HorizontallLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xLeft; x <= xReight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+              
+            
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
